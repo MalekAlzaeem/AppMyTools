9c0c070aa99e1885ddd597ed1c980e32a68dd304
MalekAlzaeem <malkmhywb036@gmail.com>
2025-08-20 23:29:31 +0300

    add widget make the direction of the elements from right to left

--- mytools/lib/Home_Screen.dart
+++ mytools/lib/Home_Screen.dart
@@ -5,6 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Directionality(
+        textDirection: TextDirection.rtl,
+        child: ,)
+    ;
   }
 }
