4d361a01fe3fe3acc217e79228f72f385f4b64ac
MalekAlzaeem <malkmhywb036@gmail.com>
2025-08-20 23:18:24 +0300

    Created my project

---
+++ mytools/android/app/src/main/kotlin/com/example/mytools/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mytools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mytools/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyTools());
+}
+
+class MyTools extends StatelessWidget {
+  const MyTools({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
