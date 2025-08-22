f444c62cc550a1fa062551b0798ddfb9b7479729
MalekAlzaeem <malkmhywb036@gmail.com>
2025-08-22 17:20:42 +0300

    add classes in page password strength

---
+++ mytools/lib/Password_Strength.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PasswordStrength extends StatefulWidget{
+  @override
+  _PasswordStrengthChecker createState() =>
+      _PasswordStrengthChecker();
+}
+
+class _PasswordStrengthChecker extends State<PasswordStrength> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
