1ba0e719131cb8685d0843db2ace9355e95c5274
MalekAlzaeem <malkmhywb036@gmail.com>
2025-08-23 21:23:28 +0300

    adding variables

--- mytools/lib/Password_Dictionary.dart
+++ mytools/lib/Password_Dictionary.dart
@@ -10,11 +10,17 @@ class PasswordDictionaryGenerator extends StatefulWidget {
  } 
 
 class _PasswordDictionaryGeneratorState extends State<PasswordDictionaryGenerator> { 
-
+  int passwordLength = 12;
+  int wordCount = 50;
+  bool includeUppercase = true;
+  bool includeLowercase = true;
+  bool includeNumbers = true; 
+  bool includeSymbols = true;
+  List<String> generatedPasswords = [];
 
   @override Widget build(BuildContext context) {
     return Scaffold( 
-      
+
     ); 
   } 
 } 
