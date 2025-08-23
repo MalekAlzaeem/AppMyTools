3cbfa506c650215652aeae11104410f3c396dcdd
MalekAlzaeem <malkmhywb036@gmail.com>
2025-08-23 21:16:13 +0300

    create the classes

--- mytools/lib/Password_Dictionary.dart
+++ mytools/lib/Password_Dictionary.dart
@@ -1,3 +1,20 @@
 import 'dart:math';
 import 'package:flutter/material.dart'; 
 import 'package:flutter/services.dart';
+
+
+
+class PasswordDictionaryGenerator extends StatefulWidget {
+  @override 
+  _PasswordDictionaryGeneratorState createState() => _PasswordDictionaryGeneratorState();
+ } 
+
+class _PasswordDictionaryGeneratorState extends State<PasswordDictionaryGenerator> { 
+
+
+  @override Widget build(BuildContext context) {
+    return Scaffold( 
+      
+    ); 
+  } 
+} 
