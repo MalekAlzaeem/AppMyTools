7824d0ea7d552a6202d9752edd1c339c6b70ce03
MalekAlzaeem <malkmhywb036@gmail.com>
2025-09-08 23:33:44 +0300

    the branch aboutApp created the About_App.dart file

---
+++ mytools/lib/About_App.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+      ),
+    );
+  }
+}
