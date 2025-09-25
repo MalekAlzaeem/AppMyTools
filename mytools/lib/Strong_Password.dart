35d711784271cf29f89a0e80825e4c6993fbc256
MalekAlzaeem <malkmhywb036@gmail.com>
2025-09-25 20:49:37 +0300

    adding the class

---
+++ mytools/lib/Strong_Password.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Screen8 extends StatelessWidget {
+bool t = false;
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(),
+    );
+  }
+}
