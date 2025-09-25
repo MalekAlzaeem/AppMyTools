3c83211ec2248b9af25aa5ade9c64ea008a4e8eb
MalekAlzaeem <malkmhywb036@gmail.com>
2025-09-25 20:12:18 +0300

    adding the class

---
+++ mytools/lib/Text_Encryption.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Screen7 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(),
+    );
+  }
+}
